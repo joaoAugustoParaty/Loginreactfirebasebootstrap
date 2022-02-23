ec15d9bb85abc2481a5ca699587527e016c07873
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-22 22:31:05 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import './App.css';
-import {Container, Row, Col} from "react-bootstrap";
-import {Routes, Route} from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 function App() {
   return (
-    <Container>
+    <Container style={{ width: "400px" }}>
       <Row>
         <Col>
-        <Routes>
-          <Route path="/" element = {<Login/>}/>
-          <Route path="/" element = {<Signup/>}/>
-        </Routes>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </UserAuthContextProvider>
         </Col>
       </Row>
     </Container>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Route>
-    <App />
-    </Route>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-reportWebVitals();
